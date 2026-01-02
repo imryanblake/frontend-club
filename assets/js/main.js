2a27d88543cc74336741f00f0d5429a1628969a5
Ryan Blake <imryanblake@gmail.com>
2026-01-01 22:11:40 -0500

    Nuevos features agregados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,30 @@
+// tooltips
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
 );
-const tooltipList = [...tooltipTriggerList].map(
-  (el) => new bootstrap.Tooltip(el)
-);
+tooltipTriggerList.forEach((el) => {
+  new bootstrap.Tooltip(el);
+});
+
+// Ventana modal
+const studentModal = document.getElementById("studentModal");
+
+studentModal.addEventListener("show.bs.modal", function (event) {
+  const box = event.relatedTarget;
+
+  document.getElementById("modalName").textContent =
+    box.getAttribute("data-name");
+
+  document.getElementById("modalRole").textContent =
+    box.getAttribute("data-role");
+
+  document.getElementById("modalAvatar").src = box.getAttribute("data-avatar");
+
+  document.getElementById("modalGithub").href = box.getAttribute("data-github");
+
+  document.getElementById("modalLinkedin").href =
+    box.getAttribute("data-linkedin");
+
+  document.getElementById("modalCodepen").href =
+    box.getAttribute("data-codepen");
+});
